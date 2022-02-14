4bc1c4de5591c6ceabf71b2ba4a78e35f4f3f8b2
Sefeo <nismoda74@gmail.com>
2022-02-14 22:04:12 +0200

    AlertColor -> GlowColor

--- api/src/main/kotlin/me/func/mod/Alert.kt
+++ api/src/main/kotlin/me/func/mod/Alert.kt
@@ -1,6 +1,7 @@
 package me.func.mod
 
 import me.func.mod.conversation.ModTransfer
+import me.func.protocol.GlowColor
 import me.func.protocol.alert.AlertColor
 import me.func.protocol.alert.NotificationButton
 import me.func.protocol.alert.NotificationData
@@ -11,7 +12,7 @@ import java.util.*
 object Alert {
 
     @JvmStatic
-    private fun toRGB(alertColor: AlertColor): Int {
+    private fun toRGB(alertColor: GlowColor): Int {
         var rgb: Int = alertColor.red
         rgb = (rgb shl 8) + alertColor.green
         rgb = (rgb shl 8) + alertColor.blue
@@ -19,7 +20,7 @@ object Alert {
     }
 
     @JvmStatic
-    fun send(player: Player, text: String, seconds: Long, frontColor: AlertColor, backGroundColor: AlertColor, chatMessage: String, vararg buttons: NotificationButton) {
+    fun send(player: Player, text: String, seconds: Long, frontColor: GlowColor, backGroundColor: GlowColor, chatMessage: String, vararg buttons: NotificationButton) {
         if(buttons.size > 2) throw RuntimeException("Too many buttons!")
 
         val notification = NotificationData(UUID.randomUUID(), "notify", text, toRGB(frontColor), toRGB(backGroundColor), seconds, buttons.asList(), chatMessage)
@@ -29,7 +30,7 @@ object Alert {
     }
 
     @JvmStatic
-    fun createButton(text: String, command: String, color: AlertColor, removeButton: Boolean, removeNotification: Boolean): NotificationButton {
+    fun createButton(text: String, command: String, color: GlowColor, removeButton: Boolean, removeNotification: Boolean): NotificationButton {
 
         return NotificationButton(text, toRGB(color), command, removeButton, removeNotification)
     }
