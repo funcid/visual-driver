245d99abeba5b3c4567d892eca1dfb4d20d8c0c9
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-02-17 20:11:19 +0300

    feat(api): add entity disguise mod api

---
+++ api/src/main/kotlin/me/func/mod/experimental/EntityDisguise.kt
@@ -0,0 +1,59 @@
+package me.func.mod.experimental
+
+import me.func.mod.conversation.ModTransfer
+import org.apiguardian.api.API
+import org.bukkit.entity.Entity
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
+
+@API(status = API.Status.EXPERIMENTAL)
+object EntityDisguise {
+    private const val channel = "kamillaova:disguise"
+
+    /**
+     * Заменяет тип энтити на клиенте у игроков из [players]
+     * @param entity Энтити, тип которой должен быть заменен
+     * @param type Новый тип энтити
+     * @param players Игроки, у которых тип будет заменен
+     */
+    @API(status = API.Status.EXPERIMENTAL)
+    fun disguise(entity: Entity, type: EntityType, vararg players: Player) {
+        ModTransfer(
+            entity.entityId, // Entity ID
+            false, // Reset
+            type.typeId.toInt() // Entity Type // TODO: Short
+        ).run { players.forEach { send(channel, it) } }
+    }
+
+    /**
+     * Заменяет тип энтити на клиенте у игроков из [players]
+     * @param entity Энтити, тип которой должен быть заменен
+     * @param type Новый тип энтити
+     * @param players Игроки, у которых тип будет заменен
+     */
+    @API(status = API.Status.EXPERIMENTAL)
+    fun disguise(entity: Entity, type: EntityType, players: List<Player>) = disguise(
+        entity, type, *players.toTypedArray()
+    )
+
+    /**
+     * Восстанавливает прежний тип энтити у игроков из [players]
+     * @param entity Энтити, тип которой будет восстановлен
+     * @param players Игоки, у которых тип энтити будет восстановлен
+     */
+    @API(status = API.Status.EXPERIMENTAL)
+    fun reset(entity: Entity, vararg players: Player) {
+        ModTransfer(
+            entity.entityId, // Entity ID
+            true // Reset
+        ).run { players.forEach { send(channel, it) } }
+    }
+
+    /**
+     * Восстанавливает прежний тип энтити у игроков из [players]
+     * @param entity Энтити, тип которой будет восстановлен
+     * @param players Игоки, у которых тип энтити будет восстановлен
+     */
+    @API(status = API.Status.EXPERIMENTAL)
+    fun reset(entity: Entity, players: List<Player>) = reset(entity, *players.toTypedArray())
+}
