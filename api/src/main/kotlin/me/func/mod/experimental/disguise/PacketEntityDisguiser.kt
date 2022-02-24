0e03b966afa84a9c0b9feca1b10a733fc93facf2
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-02-24 15:37:00 +0300

    feat(disguise): rewrite packet disguiser

--- api/src/main/kotlin/me/func/mod/experimental/disguise/PacketEntityDisguiser.kt
+++ api/src/main/kotlin/me/func/mod/experimental/disguise/PacketEntityDisguiser.kt
@@ -1,8 +1,26 @@
 package me.func.mod.experimental.disguise
 
+import net.minecraft.server.v1_12_R1.EntityLiving
+import net.minecraft.server.v1_12_R1.EntityPlayer
+import net.minecraft.server.v1_12_R1.EntityTracker
+import net.minecraft.server.v1_12_R1.EntityTypes
+import net.minecraft.server.v1_12_R1.EnumItemSlot
+import net.minecraft.server.v1_12_R1.IChunkProvider
+import net.minecraft.server.v1_12_R1.MinecraftServer
 import net.minecraft.server.v1_12_R1.PacketPlayOutEntityDestroy
+import net.minecraft.server.v1_12_R1.PacketPlayOutEntityEffect
+import net.minecraft.server.v1_12_R1.PacketPlayOutEntityEquipment
+import net.minecraft.server.v1_12_R1.PacketPlayOutEntityMetadata
+import net.minecraft.server.v1_12_R1.PacketPlayOutNamedEntitySpawn
 import net.minecraft.server.v1_12_R1.PacketPlayOutPlayerInfo
+import net.minecraft.server.v1_12_R1.PacketPlayOutSpawnEntity
 import net.minecraft.server.v1_12_R1.PacketPlayOutSpawnEntityLiving
+import net.minecraft.server.v1_12_R1.World
+import net.minecraft.server.v1_12_R1.WorldData
+import net.minecraft.server.v1_12_R1.WorldServer
+import org.bukkit.Bukkit
+import org.bukkit.World.*
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftEntity
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftLivingEntity
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer
 import org.bukkit.entity.Entity
@@ -10,40 +28,111 @@ import org.bukkit.entity.EntityType
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import java.lang.IllegalStateException
+import java.lang.UnsupportedOperationException
 
 /**
  * Not works!
  */
 internal class PacketEntityDisguiser : EntityDisguiser {
-    private val entityTypeField = PacketPlayOutSpawnEntityLiving::class.java.getDeclaredField("c").apply {
-        isAccessible = true
+    private val fakeWorld = object : WorldServer(
+        MinecraftServer.SERVER, null, WorldData(), 0, Environment.NORMAL, null
+    ) {
+        override fun n(): IChunkProvider? = null
+        override fun isChunkLoaded(i: Int, j: Int, flag: Boolean) = false
+        override fun getTracker(): EntityTracker? = null
     }
 
-    private fun PacketPlayOutSpawnEntityLiving.setEntityType(type: EntityType) = entityTypeField.set(this, type.typeId)
+    private val disguised = mutableMapOf<Entity, EntityType>()
 
-    override fun disguise(entity: Entity, type: EntityType, vararg players: Player) {
-        if (entity !is LivingEntity) throw java.lang.UnsupportedOperationException("сейчас можно ток livingentity менять тип")
-
-        players.forEach {
-            if (it !is CraftPlayer) throw IllegalStateException("игрок не инстанс CraftPlayer, wtf???")
-
-            val conn = it.handle.playerConnection
-
-            val pk = PacketPlayOutSpawnEntityLiving((entity as CraftLivingEntity).handle).apply {
-                setEntityType(type)
-            }
-
-            conn.sendPacket(PacketPlayOutEntityDestroy(intArrayOf(entity.entityId)))
-            if (entity is CraftPlayer) {
-                it.handle.playerConnection.sendPacket(
-                    PacketPlayOutPlayerInfo(
-                        PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, entity.handle
-                    )
-                )
-            }
-            conn.sendPacket(pk)
+    override fun disguise(entity: Entity, type: EntityType, vararg players: Player) { // TODO: add cache
+        val clazz = try {
+            EntityTypes.clsToTypeMap.filter { it.value.equals(type) }.keys.first()
+        } catch (e: NoSuchElementException) {
+            if (type == EntityType.PLAYER) EntityPlayer::class.java
+            else Nothing::class.java
         }
+
+        // TODO: one packet for all players???
+
+        if (EntityPlayer::class.java.isAssignableFrom(clazz)) {
+            players.forEach {
+
+                val conn = (it as CraftPlayer).handle.playerConnection
+
+                conn.sendPacket(PacketPlayOutEntityDestroy(intArrayOf(entity.entityId)))
+                conn.sendPacket(PacketPlayOutNamedEntitySpawn((entity as CraftPlayer).handle))
+                conn.sendPacket(PacketPlayOutEntityMetadata(entity.entityId, entity.handle.datawatcher, false))
+
+                EnumItemSlot.values().run { copyOfRange(2, size) }.forEach { slot ->
+                    conn.sendPacket(
+                        PacketPlayOutEntityEquipment(
+                            entity.entityId, slot, entity.handle.getEquipment(slot)
+                        )
+                    )
+                }
+
+                entity.handle.getEffects().forEach { effect ->
+                    conn.sendPacket(PacketPlayOutEntityEffect(entity.entityId, effect))
+                }
+
+                // TODO: дописать восстановку стейта
+            }
+        } else if (EntityLiving::class.java.isAssignableFrom(clazz)) {
+            players.forEach {
+                val conn = (it as CraftPlayer).handle.playerConnection
+
+                conn.sendPacket(PacketPlayOutEntityDestroy(intArrayOf(entity.entityId)))
+                conn.sendPacket(PacketPlayOutSpawnEntityLiving((entity as CraftLivingEntity).handle).apply {
+                    c = type.typeId.toInt()
+                    m = clazz.getDeclaredConstructor(World::class.java).newInstance(fakeWorld).datawatcher
+                })
+            }
+        } else {
+            // https://wiki.vg/index.php?title=Entity_metadata&oldid=14048#Objects
+            val n: Int = when (type) {
+                EntityType.BOAT -> 1
+                EntityType.DROPPED_ITEM -> 2
+                EntityType.AREA_EFFECT_CLOUD -> 3
+                EntityType.MINECART -> 10
+                EntityType.PRIMED_TNT -> 50
+                EntityType.ENDER_CRYSTAL -> 51
+                EntityType.ARROW, EntityType.TIPPED_ARROW -> 60
+                EntityType.SNOWBALL -> 61
+                EntityType.EGG -> 62
+                EntityType.FIREBALL -> 63
+                /* TODO: FireCharge (blaze projectile) */
+                EntityType.ENDER_PEARL -> 65
+                EntityType.WITHER_SKULL -> 66
+                EntityType.SHULKER_BULLET -> 67
+                EntityType.LLAMA_SPIT -> 68
+                EntityType.FALLING_BLOCK -> 70
+                EntityType.ITEM_FRAME -> 71
+                EntityType.ENDER_SIGNAL -> 72
+                EntityType.LINGERING_POTION, EntityType.SPLASH_POTION -> 73
+                EntityType.THROWN_EXP_BOTTLE -> 75
+                EntityType.FIREWORK -> 76
+                EntityType.LEASH_HITCH -> 77
+                EntityType.ARMOR_STAND -> 78
+                EntityType.EVOKER_FANGS -> 79
+                EntityType.FISHING_HOOK -> 90
+                EntityType.SPECTRAL_ARROW -> 91
+                EntityType.DRAGON_FIREBALL -> 93
+                else -> throw UnsupportedOperationException("WTF")
+            }
+
+            players.forEach {
+                val conn = (it as CraftPlayer).handle.playerConnection
+
+                conn.sendPacket(PacketPlayOutEntityDestroy(intArrayOf(entity.entityId)))
+                conn.sendPacket(PacketPlayOutSpawnEntity((entity as CraftEntity).handle, n))
+            }
+        }
+
+        disguised[entity] = type
     }
 
-    override fun reset(entity: Entity, vararg players: Player) = TODO("Not yet implemented")
+    override fun reset(entity: Entity, vararg players: Player) {
+        // TODO: Full reset players
+        disguised[entity]?.let { disguise(entity, it, *players) }
+    }
 }
