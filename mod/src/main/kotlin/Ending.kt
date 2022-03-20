67e91c772a4492bc49fd6053acc46e8b582b57e9
bezf0r <bezfors@gmail.com>
2022-03-20 14:18:38 +0200

    Добавлена подгрузка текстуры

--- mod/src/main/kotlin/Ending.kt
+++ mod/src/main/kotlin/Ending.kt
@@ -1,15 +1,17 @@
+
 import dev.xdark.clientapi.resource.ResourceLocation
 import dev.xdark.feder.NetUtil.readUtf8
 import me.func.protocol.EndStatus
 import ru.cristalix.clientapi.mod
-import ru.cristalix.uiengine.element.ContextGui
+import ru.cristalix.uiengine.UIEngine
 import ru.cristalix.uiengine.element.RectangleElement
 import ru.cristalix.uiengine.element.TextElement
+import ru.cristalix.uiengine.eventloop.animate
+import ru.cristalix.uiengine.eventloop.thenAnimate
+import ru.cristalix.uiengine.eventloop.thenWait
 import ru.cristalix.uiengine.utility.*
 
 object Ending {
-
-    private val gui = ContextGui()
 
     private lateinit var text: TextElement
     private lateinit var cup: RectangleElement
@@ -19,70 +21,89 @@ object Ending {
 
     private var filler: RectangleElement? = null
 
-    private var added = false
-
     init {
         ExternalManager.loadPaths(
             "https://i.imgur.com/mF7DWoV.png",
             "https://i.imgur.com/vxyu2tZ.png",
             "https://i.imgur.com/zUxhQ7y.png"
         )
-        if (!added || filler == null) {
+        if (filler == null) {
             filler = rectangle {
                 align = CENTER
                 origin = CENTER
                 size = V3(185.0, 103.0)
                 color = Color(255, 255, 255, 1.0)
-                offset.x -= 113
+                offset = V3(0.0, 130.0)
                 text = +text {
                     align = CENTER
                     origin = CENTER
                     color = WHITE
                     scale = V3(2.0, 2.0)
-                    offset.x += 5
+                    offset = V3(5.0, 0.0, 1.0)
                 }
                 cup = +rectangle {
                     align = CENTER
                     origin = CENTER
-                    color = Color(255, 255, 255, 0.62)
-                }
-                information = +rectangle {
-                    align = CENTER
-                    origin = CENTER
-                    size = V3(225.0, 103.0)
-                    color = Color(16, 19, 19, 0.86)
-                    offset.x += 205
-                    key = +text {
-                        align = LEFT
-                        origin = LEFT
-                        size = V3(20.0, 20.0)
-                        color = Color(255, 255, 255, 1.0)
-                        offset.x += 25
-                    }
-                    value = +text {
-                        align = RIGHT
-                        origin = RIGHT
-                        size = V3(20.0, 20.0)
-                        color = Color(255, 255, 255, 1.0)
-                        offset.x -= 25
-                    }
+                    size = V3(63.5, 104.0)
+                    color = Color(255, 255, 255)
                 }
             }
-            gui + filler!!
+            information = rectangle {
+                align = CENTER
+                origin = CENTER
+                size = V3(225.0, 103.0)
+                offset = V3(20.0, 0.0, -50.0)
+                color = Color(16, 19, 19, 0.86)
+                key = +text {
+                    align = LEFT
+                    origin = LEFT
+                    size = V3(20.0, 20.0)
+                    color = Color(255, 255, 255, 1.0)
+                    offset.x += 25
+                }
+                value = +text {
+                    align = RIGHT
+                    origin = RIGHT
+                    size = V3(20.0, 20.0)
+                    color = Color(255, 255, 255, 1.0)
+                    offset.x -= 25
+                }
+            }
         }
         App::class.mod.registerChannel("crazy:ending") {
             val endStatus = EndStatus.values()[readInt()]
             key.content = readUtf8(this)
             value.content = readUtf8(this)
+
             text.content = endStatus.title
             text.offset.z = 50.0
+
             filler?.color = Color(endStatus.red, endStatus.green, endStatus.blue, 0.86)
+
+            cup.offset.x -= endStatus.offset
+            cup.offset.z = 50.0
             cup.textureLocation = ResourceLocation.of("cache/animation", endStatus.texture)
-            cup.size = V3(63.5, 104.0)
-            cup.offset.x -= endStatus.offset // картинки топ
-            if (!added) {
-                gui.open()
-                added = true
+
+            UIEngine.overlayContext + filler!!
+            filler!!.animate(1.0) {
+                offset = V3(0.0, 0.0, 0.0)
+            }.thenWait(0.1).thenAnimate(0.0) {
+                filler!!.addChild(information)
+            }.thenAnimate(1.5) {
+                offset.x -= 113.0
+                information.offset.x += 185
+            }.thenWait(8.0).thenAnimate(0.7) {
+                information.size.x -= 40.0
+                information.offset.x -= 95
+                offset.x += 55
+            }.thenAnimate(0.7) {
+                offset.x = 0.0
+                information.offset.x = 0.0
+            }.thenWait(0.1).thenAnimate(0.0) {
+                filler!!.removeChild(information)
+            }.thenAnimate(1.0) {
+                offset.y -= 700.0
+                UIEngine.overlayContext.removeChild()
             }
         }
     }
