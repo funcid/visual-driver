2e9a2bee81a4ea124be54c19129176bc886bf2a3
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-05-07 19:03:32 +0300

    бандл мода теперь находится в build/bundle, а в апи есть жарник мода который будет подгружаться если упал стораж

--- bundler/src/main/kotlin/anime/modbundler/ProguardTask.kt
+++ bundler/src/main/kotlin/anime/modbundler/ProguardTask.kt
@@ -2,18 +2,14 @@
 
 package anime.modbundler
 
-import anime.getResource
 import org.gradle.api.JavaVersion
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
-import org.gradle.kotlin.dsl.listProperty
 import org.gradle.kotlin.dsl.property
 import proguard.gradle.ProGuardTask
 import java.nio.file.Files
