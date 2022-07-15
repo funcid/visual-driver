1c80528d92a54779d885a6f9c57ada6e64706e85
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-07-15 18:49:17 +0300

    animation api's realm id for socket client

--- api/src/main/kotlin/me/func/mod/service/Services.kt
+++ api/src/main/kotlin/me/func/mod/service/Services.kt
@@ -11,7 +11,9 @@ import ru.cristalix.core.invoice.IInvoiceService
 import ru.cristalix.core.invoice.InvoiceService
 import ru.cristalix.core.network.ISocketClient
 import ru.cristalix.core.network.SocketClient
+import ru.cristalix.core.realm.RealmId
 import java.lang.System.getenv
+import java.lang.reflect.Constructor
 import java.util.UUID
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
@@ -22,7 +24,15 @@ object Services {
             connect(
                 getenv("TOWER_IP"),
                 getenv("TOWER_PORT").toInt(),
-                CoreCredentials.fromEnvironment()
+                CoreCredentials::class.java.getDeclaredConstructor(
+                    RealmId::class.java,
+                    String::class.java,
+                    String::class.java
+                ).apply { isAccessible = true }.newInstance(
+                    RealmId.of("ANIM", 600 + (Math.random() * 300).toInt()),
+                    getenv("TOWER_LOGIN"),
+                    getenv("TOWER_PASSWORD")
+                )
             )
             waitForHandshake()
         }
