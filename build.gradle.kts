2977fcb17137f1f67eef650453c1da8688ff5bc3
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-08-17 15:31:26 +0300

    [ci skip] bump version to 2.2.29

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ allprojects {
      * Подробнее про семантическое версионирование:
      *     https://semver.org/lang/ru/
      */
-    version = "2.2.28"
+    version = "2.2.29"
 }
 
 subprojects {
