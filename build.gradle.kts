6a5910eb6d42ca1eb80c1ef7e17d004621675e40
iLisov <lisov.inc@mail.ru>
2022-08-21 04:11:45 +0700

    bump 2.4.19

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ allprojects {
      * Подробнее про семантическое версионирование:
      *     https://semver.org/lang/ru/
      */
-    version = "2.4.18"
+    version = "2.4.19"
 }
 
 subprojects {
