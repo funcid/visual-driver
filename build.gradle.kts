9ee88daa1291e6e4510ab97e7df1b96a60915483
reidjJava <sp1.ke@mail.ru>
2022-09-10 16:29:53 +0300

    Исправлена загрузка диалогов

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ allprojects {
      * Подробнее про семантическое версионирование:
      *     https://semver.org/lang/ru/
      */
-    version = "2.7.25"
+    version = "2.7.26"
 }
 
 subprojects {
