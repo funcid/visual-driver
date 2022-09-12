665fa59afb26af81419129eebe56a15cfe9da146
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-09-12 17:57:52 +0300

    bump 2.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ allprojects {
      * Подробнее про семантическое версионирование:
      *     https://semver.org/lang/ru/
      */
-    version = "2.7.39"
+    version = "2.7.40"
 }
 
 subprojects {
