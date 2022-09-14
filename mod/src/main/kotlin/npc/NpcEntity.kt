b807348221f2b49002bcb7828accd77c88361fda
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-09-15 00:08:05 +0300

    [ci skip] Экономим 430 байт ради камилы

--- mod/src/main/kotlin/npc/NpcEntity.kt
+++ mod/src/main/kotlin/npc/NpcEntity.kt
@@ -4,7 +4,7 @@ import dev.xdark.clientapi.entity.EntityLivingBase
 import me.func.protocol.world.npc.NpcData
 import java.util.UUID
 
-data class NpcEntity(
+class NpcEntity(
     val uuid: UUID,
     val data: NpcData,
     val entity: EntityLivingBase
