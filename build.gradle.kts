357d4f045035e8326dce924271176f0d0c4bc61d
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-10-10 00:09:25 +0300

    Забыл api dump, bump 3.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ allprojects {
      * Подробнее про семантическое версионирование:
      *     https://semver.org/lang/ru/
      */
-    version = "3.7.0"
+    version = "3.7.1"
 }
 
 subprojects {
