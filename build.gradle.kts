ad95581574f02e137bf6b1cef5bac2025a33c2f6
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-10-15 15:00:50 +0300

    Update api.api, pump 3.8.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ allprojects {
      * Подробнее про семантическое версионирование:
      *     https://semver.org/lang/ru/
      */
-    version = "3.8.5"
+    version = "3.8.6"
 }
 
 subprojects {
