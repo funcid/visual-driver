b8d0cf3f7f4259f8abcbe1ed0abf9c8ab07ebaba
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-10-15 15:00:08 +0300

    Забыл api dump, bunp 3.8.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ allprojects {
      * Подробнее про семантическое версионирование:
      *     https://semver.org/lang/ru/
      */
-    version = "3.8.4"
+    version = "3.8.5"
 }
 
 subprojects {
