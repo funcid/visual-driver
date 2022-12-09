2fa398c506c9c93b8dbe9ab1e0368a677f6d1c3c
lisov <lisov.inc@mail.ru>
2022-12-09 12:55:41 +0700

    fix

--- mod/src/main/kotlin/standard/ui/OverlayText.kt
+++ mod/src/main/kotlin/standard/ui/OverlayText.kt
@@ -49,6 +49,29 @@ class OverlayText {
         UIEngine.overlayContext + this
     }
 
+    private val top: TextElement by lazyText {
+        origin = TOP
+        align = TOP
+        shadow = true
+        offset.y += 2
+        UIEngine.overlayContext + this
+    }
+
+    private val centerLeft: TextElement by lazyText {
+        origin = LEFT
+        align = LEFT
+        shadow = true
+        offset.x += 3
+        UIEngine.overlayContext + this
+    }
+    private val centerRight: TextElement by lazyText {
+        origin = RIGHT
+        align = RIGHT
+        shadow = true
+        offset.x -= 3
+        UIEngine.overlayContext + this
+    }
+
     init {
         mod.registerChannel("anime:overlay") {
             when (Position.values()[readInt()]) {
@@ -56,10 +79,15 @@ class OverlayText {
                 Position.BOTTOM_LEFT -> bottomLeft.content = NetUtil.readUtf8(this)
                 Position.TOP_RIGHT -> topRight.content = NetUtil.readUtf8(this)
                 Position.TOP_LEFT -> topLeft.content = NetUtil.readUtf8(this)
+                Position.RIGHT -> centerRight.content = NetUtil.readUtf8(this)
+                Position.LEFT -> centerLeft.content = NetUtil.readUtf8(this)
+                Position.TOP -> top.content = NetUtil.readUtf8(this)
+                else -> {}
             }
-            registerHandler<GameLoop> {
-                bottomLeft.enabled = UIEngine.clientApi.minecraft().currentScreen() !is ChatScreen
-            }
+        }
+        registerHandler<GameLoop> {
+            bottomLeft.enabled = UIEngine.clientApi.minecraft().currentScreen() !is ChatScreen
+            top.enabled = UIEngine.clientApi.minecraft().currentScreen() !is ChatScreen
         }
     }
 }
